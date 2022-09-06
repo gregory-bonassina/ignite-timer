83d056aca51cd24b614a05d3d86ff94a188b91fa
Gregory Bonassina <bonassina3@gmail.com>
2022-09-05 21:47:52 -0300

    finish module

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { CycleContextProvider } from './context/CyclesContext'
+
+export function App() {
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+                <CycleContextProvider>
+                    <Router />
+                </CycleContextProvider>
+            </BrowserRouter>
+
+            <GlobalStyle />
+        </ThemeProvider>
+    )
+}
